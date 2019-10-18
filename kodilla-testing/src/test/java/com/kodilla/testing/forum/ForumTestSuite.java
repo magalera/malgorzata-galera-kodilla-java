d8bebcf9102851597d96d8c38be5eeff6ace34c4
Małgorzata Galera <malgorzata.galera.92@gmail.com>
2019-10-18 20:12:58 +0200

    Created Forum and tests

--- kodilla-testing/src/test/java/com/kodilla/testing/forum/ForumTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/ForumTestSuite.java
@@ -1,46 +1,156 @@
 package com.kodilla.testing.forum;
 
-import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.forum.ForumComment;
+import com.kodilla.testing.forum.ForumPost;
+import com.kodilla.testing.forum.ForumUser;
 import org.junit.*;
 
 public class ForumTestSuite {
-    @Before
-    public void before() {
-        System.out.println("Test case: begin");
-    }
-
-    @After
-    public void after() {
-        System.out.println("Test case: end");
-    }
+    private static int testCounter = 0;
 
     @BeforeClass
-    public static void beforeClass() {
-        System.out.println("Test suit: begin");
+    public static void beforeAllTests() {
+        System.out.println("This is the beginning of tests.");
     }
 
     @AfterClass
-    public static void afterClass() {
-        System.out.println("Test suit: end");
+    public static void afterAllTests() {
+        System.out.println("All tests are finished.");
     }
 
+    @Before
+    public void beforeEveryTest() {
+        testCounter++;
+        System.out.println("Preparing to execute test #" + testCounter);
+    }
+
+    //Test 1: sprawdzający, czy po dodaniu nowego postu liczba postów wynosi 1.
     @Test
-    public void testCaseUsername() {
+    public void testAddPost() {
         //Given
-        SimpleUser simpleUser = new SimpleUser("theForumUser", "Tommy Sullivan");
+        ForumUser forumUser = new ForumUser("mrsmith", "John Smith");
+
         //When
-        String result = simpleUser.getUsername();
+        forumUser.addPost("mrsmith",
+                "Hello everyone, this is my first contribution here!");
+
         //Then
-        System.out.println("Testing " + result);
-        Assert.assertEquals("theForumUser", result);
+        Assert.assertEquals(1, forumUser.getPostsQuantity());
     }
 
+    //Test 2: sprawdzający, czy po dodaniu nowego komentarza liczba komentarzy wynosi 1.
     @Test
-    public void testCaseRealName() {
-        SimpleUser simpleUser = new SimpleUser("theForumUser", "Tommy Sullivan");
-        String result = simpleUser.getRaelName();
-        System.out.println("Testing " + result);
-        Assert.assertEquals("Tommy Sullivan", result);
+    public void testAddComment() {
+        //Given
+        ForumUser forumUser = new ForumUser("mrsmith", "John Smith");
+        ForumPost thePost = new ForumPost("Hello everyone, " +
+                "this is my first contribution here!", "mrsmith");
+
+        //When
+        forumUser.addComment(thePost, ",mrsmith", "Thank you for all good words!");
+
+        //Then
+        Assert.assertEquals(1, forumUser.getCommentsQuantity());
+    }
+
+    //Test 3: sprawdzający, czy post pobrany z klasy ForumUser jest taki sam, jak post, który był wstawiany do klasy.
+    @Test
+    public void testGetPost() {
+        ForumUser forumUser = new ForumUser("mrsmith", "John Smith");
+        ForumPost thePost = new ForumPost("Hello everyone, " +
+                "this is my first contribution here!", "mrsmith");
+        forumUser.addPost(thePost.getAuthor(), thePost.getPostBody());
+
+        //When
+        ForumPost retrievedPost = forumUser.getPost(0);
+
+        //Then
+        Assert.assertEquals(thePost, retrievedPost);
+    }
+
+    //Test 4: sprawdzający, czy komentarz pobrany z klasy ForumUser jest taki sam, jak komentarz, który był wstawiany do klasy.
+    @Test
+    public void testGetComment() {
+        //Given
+        ForumUser forumUser = new ForumUser("mrsmith", "John Smith");
+        ForumPost thePost = new ForumPost("Hello everyone, " +
+                "this is my first contribution here!", "mrsmith");
+        ForumComment theComment = new ForumComment(thePost, "mrsmith", "Thank you for all good words!");
+        forumUser.addComment(thePost, thePost.getAuthor(), theComment.getCommentBody());
+
+        //When
+        ForumComment retrievedComment;
+        retrievedComment = forumUser.getComment(0);
+
+        //Then
+        Assert.assertEquals(theComment, retrievedComment);
+    }
+
+    //Test 5: sprawdzający, czy próba usunięcia nieistniejącego posta skończy się zwróceniem wyniku false.
+    @Test
+    public void testRemovePostNotExisting() {
+        //Given
+        ForumUser forumUser = new ForumUser("mrsmith", "John Smith");
+        ForumPost thePost = new ForumPost("Hello everyone, " +
+                "this is my first contribution here!", "mrsmith");
+
+        //When
+        boolean result = forumUser.removePost(thePost);
+
+        //Then
+        Assert.assertFalse(result);
+    }
+
+    //Test 6: sprawdzający, czy próba usunięcia nieistniejącego komentarza skończy się zwróceniem wyniku false.
+    @Test
+    public void testRemoveCommentNotExisting() {
+        //Given
+        ForumUser forumUser = new ForumUser("mrsmith", "John Smith");
+        ForumPost thePost = new ForumPost("Hello everyone, " +
+                "this is my first contribution here!", "mrsmith");
+        ForumComment theComment = new ForumComment(thePost,  ",mrsmith", "Thank you for all good words!");
+
+        //When
+        boolean result = forumUser.removeComment(theComment);
+
+        //Then
+        Assert.assertFalse(result);
+    }
+
+    //Test 7: weryfikujący, czy udało się usunąć wybrany post z klasy.
+    @Test
+    public void testRemovePost() {
+        //Given
+        ForumUser forumUser = new ForumUser("mrsmith", "John Smith");
+        ForumPost thePost = new ForumPost("Hello everyone, " +
+                "this is my first contribution here!", "mrsmith");
+        forumUser.addPost(thePost.getAuthor(), thePost.getPostBody());
+
+        //When
+        boolean result = forumUser.removePost(thePost);
+
+        //Then
+        Assert.assertTrue(result);
+        Assert.assertEquals(0, forumUser.getPostsQuantity());
+
+    }
+
+    //Test 8: weryfikujący, czy udało się usunąć wybrany komentarz z klasy.
+    @Test
+    public void testRemoveComment() {
+        //Given
+        ForumUser forumUser = new ForumUser("mrsmith", "John Smith");
+        ForumPost thePost = new ForumPost("Hello everyone, " +
+                "this is my first contribution here!", "mrsmith");
+        ForumComment theComment = new ForumComment(thePost, "mrsmith", "Thank you for all good words!");
+        forumUser.addComment(thePost, theComment.getAuthor(), theComment.getCommentBody());
+
+        //When
+        boolean result = forumUser.removeComment(theComment);
+
+        //Then
+        Assert.assertTrue(result);
+        Assert.assertEquals(0, forumUser.getCommentsQuantity());
     }
 }
 
