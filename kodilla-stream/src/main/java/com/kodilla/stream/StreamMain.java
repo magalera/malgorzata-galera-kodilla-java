1cc2c99320f7bb83b8e83c4b325600e0d94e0e07
Małgorzata Galera <malgorzata.galera.92@gmail.com>
2019-10-24 18:02:50 +0200

    Created Text decorator app with lambda expression

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,27 +1,18 @@
 package com.kodilla.stream;
 
+import com.kodilla.stream.beautifier.PoemBeautifier;
 import com.kodilla.stream.lambda.*;
 import com.kodilla.stream.reference.FunctionalCalculator;
 
 public class StreamMain {
     public static void main(String[] args) {
 
-        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
 
-        expressionExecutor.executeExpression(10, 5, (a, b) -> (a + b));
-        expressionExecutor.executeExpression(10, 5, (a, b) -> (a - b));
-        expressionExecutor.executeExpression(10, 5, (a, b) -> (a * b));
-        expressionExecutor.executeExpression(10, 5, (a, b) -> (a / b));
-
-        expressionExecutor.executeExpression(20, 10, (FunctionalCalculator::addAToB));
-        expressionExecutor.executeExpression(20, 10, (FunctionalCalculator::divideAByB));
-        expressionExecutor.executeExpression(20, 10, (FunctionalCalculator::multiplyAByB));
-        expressionExecutor.executeExpression(20, 10, (FunctionalCalculator::subBFromA));
-
-        //Processor processor = new Processor();
-        //Executor codeToExecute = ()-> System.out.println("This is an example text.");
-        //processor.execute(codeToExecute);
-        // albo bez tworzenia zmiennej:
-        // processor.execute(()-> System.out.println("This is an example text."););
+        poemBeautifier.beautify("capslook", (text -> text.toUpperCase()));
+        poemBeautifier.beautify("emoticon", (text -> ":-)_" + text + "_(-:"));
+        poemBeautifier.beautify("Beautiful tekst", (text -> "*&*&*&*&*&*" + text + "*&*&*&*&*&*"));
+        poemBeautifier.beautify("rock and roll", (text -> "Rock&Roll"));
+        poemBeautifier.beautify("Hello, how are you?", (text -> text + " ;-) :-D :-)"));
     }
 }
