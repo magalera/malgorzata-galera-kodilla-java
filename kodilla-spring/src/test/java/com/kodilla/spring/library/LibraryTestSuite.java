7254b2d6e9b579b4f559ed406347c87ee164dc80
Małgorzata Galera <malgorzata.galera.92@gmail.com>
2020-01-12 22:15:01 +0100

    Spring course examples

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/library/LibraryTestSuite.java
@@ -0,0 +1,49 @@
+package com.kodilla.spring.library;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.Arrays;
+
+public class LibraryTestSuite {
+
+    @Test
+    public void testSaveToDb() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext(LibraryConfig.class);
+        Library library = context.getBean(Library.class);
+
+        //When
+        library.saveToDb();
+
+        //Then
+        //do nothing
+    }
+
+    @Test
+    public void testLoadFromDb() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Library library = context.getBean(Library.class);
+
+        //When
+        library.loadFromDb();
+
+        //Then
+        //do nothing
+    }
+
+    @Test
+    public void testContext(){
+        //Given
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.kodilla.spring");
+
+        //When & Then
+        System.out.println("===== Beans list: ==== >>");
+        Arrays.stream(context.getBeanDefinitionNames())
+                .forEach(System.out::println);
+        System.out.println("<< ===== Beans list ====");
+    }
+}
