524b414438148b63476b812a71e0eda41501abcd
Małgorzata Galera <malgorzata.galera.92@gmail.com>
2020-02-29 15:25:27 +0100

    One to many entity relation

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/Task.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/Task.java
@@ -1,5 +1,7 @@
 package com.kodilla.hibernate.task;
 
+import com.kodilla.hibernate.tasklist.TaskList;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.util.Date;
@@ -12,6 +14,7 @@ public final class Task {
     private Date created;
     private int duration;
     private TaskFinancialDetails taskFinancialDetails;
+    private TaskList taskList;
 
     public Task() {
     }
@@ -52,6 +55,12 @@ public final class Task {
         return taskFinancialDetails;
     }
 
+    @ManyToOne
+    @JoinColumn(name = "TASKLIST_ID")
+    public TaskList getTaskList() {
+        return taskList;
+    }
+
     private void setId(int id) {
         this.id = id;
     }
@@ -72,4 +81,7 @@ public final class Task {
         this.taskFinancialDetails = taskFinancialDetails;
     }
 
+    public void setTaskList(TaskList taskList) {
+        this.taskList = taskList;
+    }
 }
--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
@@ -1,5 +1,7 @@
 package com.kodilla.hibernate.tasklist.dao;
 
+import com.kodilla.hibernate.task.Task;
+import com.kodilla.hibernate.task.TaskFinancialDetails;
 import com.kodilla.hibernate.tasklist.TaskList;
 import org.junit.Assert;
 import org.junit.Test;
@@ -8,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @RunWith(SpringRunner.class)
@@ -15,23 +18,49 @@ import java.util.List;
 public class TaskListDaoTestSuite {
     @Autowired
     private TaskListDao taskListDao;
-    private static final String TASK_LIST_NAME = "My list";
+    private static final String LIST_NAME = "My list";
     private static final String DESCRIPTION = "Test: Learn Hibernate";
 
     @Test
     public void testFindByListName() {
         //Given
-        TaskList taskList = new TaskList(TASK_LIST_NAME, DESCRIPTION);
+        TaskList taskList = new TaskList(LIST_NAME, DESCRIPTION);
 
         //When
         taskListDao.save(taskList);
 
         //Then
         int id = taskList.getId();
-        List<TaskList> taskLists = taskListDao.findByListName(TASK_LIST_NAME);
+        List<TaskList> taskLists = taskListDao.findByListName(LIST_NAME);
         Assert.assertEquals(1, taskLists.size());
-        Assert.assertEquals(TASK_LIST_NAME, taskLists.get(0).getListName());
+        Assert.assertEquals(LIST_NAME, taskLists.get(0).getListName());
         Assert.assertEquals(DESCRIPTION, taskLists.get(0).getDescription());
+
+        //CleanUp
+        taskListDao.deleteById(id);
+    }
+
+    @Test
+    public void testTaskListDaoSaveWithTasks() {
+        //Given
+        Task task = new Task("Test: Learn Hibernate", 14);
+        Task task2 = new Task("Test: Write some entities", 3);
+        TaskFinancialDetails tfd = new TaskFinancialDetails(new BigDecimal(20), false);
+        TaskFinancialDetails tfd2 = new TaskFinancialDetails(new BigDecimal(10), false);
+        task.setTaskFinancialDetails(tfd);
+        task2.setTaskFinancialDetails(tfd2);
+        TaskList taskList = new TaskList(LIST_NAME, "ToDo tasks");
+        taskList.getTasks().add(task);
+        taskList.getTasks().add(task2);
+        task.setTaskList(taskList);
+        task2.setTaskList(taskList);
+
+        //When
+        taskListDao.save(taskList);
+        int id = taskList.getId();
+
+        //Then
+        Assert.assertNotEquals(0, id);
 
         //CleanUp
         taskListDao.deleteById(id);
