ba4e23ac19c21cb32cd076e77686f5bb6fbba1d0
Małgorzata Galera <malgorzata.galera.92@gmail.com>
2020-05-01 20:41:25 +0200

    Add Observer

---
+++ kodilla-patterns2/src/main/java/com/kodilla/kodillapatterns2/observer/forum/Observable.java
@@ -0,0 +1,9 @@
+package com.kodilla.kodillapatterns2.observer.forum;
+
+public interface Observable {
+    void registerObserver(Observer observer);
+
+    void notifyObservers();
+
+    void removeObserver(Observer observer);
+}
